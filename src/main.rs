9336091ff8a7ded9c38346fafaf65e5e6d7699c0
R Wood <archaurore@gmail.com>
2019-02-23 20:36:47 +0100

    added a fibonacci calculator

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn main() {
     let t_in_c = convert_f_to_c(t_in_f);
 
     println!("If a temperature in F is {}, temperature in C is {}", t_in_f, t_in_c);
+    let n:usize = 12;
+    let f:usize = calculate_fibonnaci(n);
+
+    println!("The fibonnaci of n = {} is {}", n, f);
 }
 
 fn convert_c_to_f(t: i32) -> i32 {
@@ -16,4 +20,25 @@ fn convert_c_to_f(t: i32) -> i32 {
 
 fn convert_f_to_c(t: i32) -> i32 {
     (t - 32) * 5/9
+}
+
+fn calculate_fibonnaci(n: usize) -> usize {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        let mut n_1:usize = 0;
+        let mut n_2:usize = 1;
+        let mut counter:usize = 1;
+        let mut f:usize = 0;
+
+        while counter < n {
+            f = n_1 + n_2;
+            n_1 = n_2;
+            n_2 = f;
+            counter = counter + 1;
+        }
+        f
+    }
 }
